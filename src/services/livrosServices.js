a61d890aa9d0460ce2ca997c0094f2e7e5ac1e40
Aluno SENAI <Aluno@SN-391648.edu.fiesc.com.br>
2026-03-19 08:48:04 -0300

    Adicionando pastas e arquivos a API

---
+++ src/services/livrosServices.js
@@ -0,0 +1,27 @@
+const livros = [
+    {
+        id: 1,
+        autor: "Antonio Santa Ana",   
+        disponivel: true,
+        titulo: "Os Olhos do Cão Siberiano"
+    },
+    {
+        id: 2,
+        autor: "Lewis Carroll",   
+        disponivel: true,
+        titulo: "Alice no País das Maravilhas"
+    },
+    {
+        id: 3,
+        autor: "Anne Frank",   
+        disponivel: true,
+        titulo:"O Diário de Anne Frank"
+    } 
+    ]  
+    
+    const listartodosOsLivros = async () => {
+    
+    return acervo;
+    }
+    
+    module.export = {listartodosOsLivros}
