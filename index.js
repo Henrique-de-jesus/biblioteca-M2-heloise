e6dfc67a959c85844b1fa1022bb5a2d76c8cca01
HELOÍSE DE JESUS <heloise_jesus@estudante.sesisenai.org.br>
2026-03-30 11:14:01 -0300

    Reajustando estruturas

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const routes = require('./src/routes/index.routes');
 const app = express();
-
+const logger = require('./src/middlewares/logger.middleware')
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  next();
-});
+app.use(logger);
 
 // Utilizando as rotas
 app.use(routes);
